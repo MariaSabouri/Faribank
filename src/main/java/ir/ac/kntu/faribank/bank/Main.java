048c8c859a2f135f02072b3a317e0387537ed596
MariaSabouri <160609587+MariaSabouri@users.noreply.github.com>
2024-07-12 01:19:16 +0330

    fix UI base

--- src/main/java/ir/ac/kntu/faribank/bank/Main.java
+++ src/main/java/ir/ac/kntu/faribank/bank/Main.java
@@ -1,12 +1,21 @@
-package ir.ac.kntu.faribank;
+package ir.ac.kntu.faribank.bank;
 
+import ir.ac.kntu.faribank.FXML_Loader;
 import ir.ac.kntu.faribank.bank.Errors.InvalidInputExeption;
 import ir.ac.kntu.faribank.menu.MWelcome;
 import ir.ac.kntu.faribank.menu.client.MLoginClient;
 import ir.ac.kntu.faribank.util.ScannerWrapper;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+
     public static void main(String[] args) {
+        launch();
         MWelcome.Role option = null;
         MWelcome.getInstance().printMenu();
 
@@ -33,4 +42,13 @@ public class Main {
 
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(FXML_Loader.loadURL("welcome.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("FaridBank");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
