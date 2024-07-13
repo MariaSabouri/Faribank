e8e45a854f3c25a7f12a7237b006cf8307a5c050
MariaSabouri <160609587+MariaSabouri@users.noreply.github.com>
2024-07-13 10:49:20 +0330

    develop Authentication.fxml

--- src/main/java/ir/ac/kntu/faribank/Controller/client/AuthenticationController.java
+++ src/main/java/ir/ac/kntu/faribank/Controller/client/AuthenticationController.java
@@ -1,13 +1,32 @@
 package ir.ac.kntu.faribank.Controller.client;
 
+import ir.ac.kntu.faribank.Controller.ProjectFX;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AuthenticationController implements Initializable {
+
+    @FXML
+    private Button LogOutButton;
+
+    @FXML
+    private WebView waitSVG;
+    private static Stage stage;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        LogOutButton.setOnMouseClicked(mouseEvent -> LogOutButtonHandler());
 
+    }
+
+    private void LogOutButtonHandler() {
+        stage=(Stage) LogOutButton.getScene().getWindow();
+        ProjectFX.changingscene(stage,"Authentication.fxml");
     }
 }
