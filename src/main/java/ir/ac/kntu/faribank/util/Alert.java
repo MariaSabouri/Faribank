6b56b868df1265467c4de8a8065ffa9549ce3779
MariaSabouri <160609587+MariaSabouri@users.noreply.github.com>
2024-07-14 12:54:17 +0330

    develop Alert.java

--- src/main/java/ir/ac/kntu/faribank/util/Alert.java
+++ src/main/java/ir/ac/kntu/faribank/util/Alert.java
@@ -8,4 +8,11 @@ public class Alert {
         alert.setContentText(errMessage);
         alert.showAndWait();
     }
+    public static void showingError(String errorHeader,String errMessage){
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(errorHeader);
+        alert.setContentText(errMessage);
+        alert.showAndWait();
+    }
 }
