1148bd72d5e9c0ea729b0730ceb9d276221d2e4c
MariaSabouri <160609587+MariaSabouri@users.noreply.github.com>
2024-07-15 16:55:12 +0330

    develop transfer class

--- src/main/java/ir/ac/kntu/faribank/Controller/client/transfer/PaymentConfirmationController.java
+++ src/main/java/ir/ac/kntu/faribank/Controller/client/transfer/PaymentConfirmationController.java
@@ -1,15 +1,27 @@
 package ir.ac.kntu.faribank.Controller.client.transfer;
 
 import ir.ac.kntu.faribank.Controller.ProjectFX;
+import ir.ac.kntu.faribank.bank.client.Contact;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PaymentConfirmationController implements Initializable {
+
+    @FXML
+    private Label ToLabel;
+
+    @FXML
+    private Label TransferLabel;
+
+    @FXML
+    private Label desAccountLabel;
+
     @FXML
     private Button backButton;
 
@@ -17,15 +29,32 @@ public class PaymentConfirmationController implements Initializable {
     private Button submitButton;
 
     private static Stage stage;
+
+    private static String destinationAccount;
+    private static String TransferAmount;
+    private static Contact contactChosen;
+
+    public static void setContctChosen(String destinationAccount, String transferAmount, Contact contactChosen) {
+        PaymentConfirmationController.contactChosen=contactChosen;
+        PaymentConfirmationController.destinationAccount=destinationAccount;
+        PaymentConfirmationController.TransferAmount=transferAmount;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         backButton.setOnMouseClicked(mouseEvent -> backButtonHandler());
         submitButton.setOnMouseClicked(mouseEvent -> submitButtonHandler());
 
+        ToLabel.setText(contactChosen.getFirstName()+" "+contactChosen.getLastName());
+        desAccountLabel.setText(destinationAccount);
+        TransferLabel.setText(TransferAmount);
+
+
     }
 
     private void submitButtonHandler() {
         stage=(Stage) submitButton.getScene().getWindow();
+        //todo
         ProjectFX.changingscene(stage,"clientFXML/transfer/TransferTransaction.fxml");
     }
 
--- src/main/java/ir/ac/kntu/faribank/Controller/client/transfer/TransferController.java
+++ src/main/java/ir/ac/kntu/faribank/Controller/client/transfer/TransferController.java
@@ -1,29 +1,42 @@
 package ir.ac.kntu.faribank.Controller.client.transfer;
 
 import ir.ac.kntu.faribank.Controller.ProjectFX;
+import ir.ac.kntu.faribank.Controller.client.HomeController;
+import ir.ac.kntu.faribank.bank.Errors.InsufficientFundsException;
+import ir.ac.kntu.faribank.bank.Errors.InvalidAmountException;
+import ir.ac.kntu.faribank.bank.Errors.InvalidInputException;
+import ir.ac.kntu.faribank.bank.Errors.NotFoundException;
+import ir.ac.kntu.faribank.bank.client.Contact;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class TransferController implements Initializable {
 
     @FXML
-    private TextField SelectDestinationAccount;
+    private ChoiceBox<String> ListOfRecentContactsChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> contactListChoiceBox;
+
+    @FXML
+    private Label SelectDestinationAccount;
 
     @FXML
     private TextField TransferAmountTextField;
-
-    @FXML
-    private ChoiceBox<?> ListOfRecentContactsChoiceBox;
-
-    @FXML
-    private ChoiceBox<?> contactListChoiceBox;
 
     @FXML
     private Button backButton;
@@ -33,16 +46,66 @@ public class TransferController implements Initializable {
 
     private static Stage stage;
 
+    private static Contact contactChosen;
+
+    public static void setContactChosen(Contact contactChosen) {
+        TransferController.contactChosen = contactChosen;
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         backButton.setOnMouseClicked(mouseEvent -> backButtonHandler());
         submitButton.setOnMouseClicked(mouseEvent -> submitButtonHandler());
-        //todo
+
+        ListOfRecentContactsChoiceBox.getItems().add("Select destination");
+        contactListChoiceBox.getItems().add("Select destination");
+
+
+        //recent contacts list
+        ArrayList<Contact> recentContacts=HomeController.getClient().getRecentContacts();
+        Collections.sort(recentContacts);
+        for (Contact contact:recentContacts){
+
+            ListOfRecentContactsChoiceBox.getItems().add(contact.getFirstName()+" "+contact.getLastName());
+
+            ListOfRecentContactsChoiceBox.setOnAction(event -> {
+                String value=ListOfRecentContactsChoiceBox.getValue();
+                if (!value.equals("Select destination")){
+                    SelectDestinationAccount.setText(contact.getAccountNumber());
+                    setContactChosen(contact);
+                }else {
+                    SelectDestinationAccount.setText(""); // Set default text to nothing
+                }
+            });
+
+        }
+
+
+        //contacts list
+        for (Contact contact: HomeController.getClient().geContacts()) {
+
+            contactListChoiceBox.getItems().add(contact.getFirstName() + " " + contact.getLastName());
+
+            contactListChoiceBox.setOnAction(event -> {
+                String value = contactListChoiceBox.getValue();
+                if (!value.equals("Select destination")) {
+                    SelectDestinationAccount.setText(contact.getAccountNumber());
+                    setContactChosen(contact);
+                }else {
+                    SelectDestinationAccount.setText(""); // Set default text to nothing
+                }
+            });
+        }
     }
 
     private void submitButtonHandler() {
         stage=(Stage) submitButton.getScene().getWindow();
-        //todo
+
+        String destinationAccount =SelectDestinationAccount.getText();
+        String TransferAmount=TransferAmountTextField.getText();
+        PaymentConfirmationController.setContctChosen(destinationAccount,TransferAmount,contactChosen);
+
         ProjectFX.changingscene(stage,"clientFXML/transfer/PaymentConfirmation.fxml");
     }
 
