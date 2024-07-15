77657410f920b56cad87c9414c0fbdbc9c701e8c
MariaSabouri <160609587+MariaSabouri@users.noreply.github.com>
2024-07-15 19:03:24 +0330

    develop transfer class

---
+++ src/main/java/ir/ac/kntu/faribank/Controller/client/support/feedbackController.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu.faribank.Controller.client.support;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class feedbackController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
